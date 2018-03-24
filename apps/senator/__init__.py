70524f1e815bde5a007474c6be5ce7cb9b1188fe
Gilson Paulino <gilsonbp@gmail.com>
2018-03-24 13:40:03 -0300

    Creating the senator module

---
+++ apps/senator/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Gilson Paulino"
+__date__ = "Created by 24/03/18"
+__copyright__ = "Copyright 2018"
+__email__ = "gilsonbp@gmail.com"
+
+default_app_config = 'apps.senator.apps.SenatorConfig'
---
+++ apps/senator/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+__author__ = "Gilson Paulino"
+__date__ = "Created by 24/03/18"
+__copyright__ = "Copyright 2018"
+__email__ = "gilsonbp@gmail.com"
+
+
+class SenatorConfig(AppConfig):
+    name = 'apps.senator'
+    verbose_name = 'Senator'
